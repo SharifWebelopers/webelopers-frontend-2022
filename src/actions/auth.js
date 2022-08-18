1c2fece8839a6b28c670488e11f9f6eb06d958f2
Ali asghar Ghanati <a.a.ghanati@gmail.com>
2022-08-18 12:18:39 +0430

    - sso completed! :)

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -15,3 +15,7 @@ export const requestEmailVerification = (params) => {
 export const login = (params) => {
   return axios().post(`/accounts/login/`, params);
 };
+
+export const sendSocialAuthToken = (socialNetwork, params) => {
+  return axios().post(`/socialoauth/${socialNetwork}/`, params);
+};
--- src/components/OAuth/index.tsx
+++ src/components/OAuth/index.tsx
@@ -1,22 +1,46 @@
 import { Divider, IconButton } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/router";
+
 import { useGoogleLogin } from "@react-oauth/google";
 // @ts-ignore
 import GitHubLogin from "react-github-login";
 
+import { sendSocialAuthToken } from "../../actions/auth";
+
 import styles from "./OAuth.module.scss";
 
 function OAuth() {
-  const responseGoogle = (res: any) => {
+  const router = useRouter();
+
+  const responseGoogle = async (res: any) => {
     console.log("googla googla", res);
+    sendSocialAuthToken("google", { auth_token: res.access_token })
+      .then((res) => {
+        // maybe so some other things? (store token for example)
+        router.push("/");
+      })
+      .catch((err) => {
+        // show some error (for github with private email need to show suitable error)
+      });
   };
+
   const responseGithub = (res: any) => {
-    console.log("githuuuuuuub", res);
+    sendSocialAuthToken("github", { auth_token: res.code })
+      .then((res) => {
+        // maybe so some other things? (store token for example)
+        router.push("/");
+      })
+      .catch((err) => {
+        // show some error (for github with private email need to show suitable error)
+      });
   };
 
   const googleLogin = useGoogleLogin({
     onSuccess: responseGoogle,
-    onError: responseGoogle,
+    onError: () => {
+      // show some error
+    },
   });
 
   return (
@@ -39,9 +63,12 @@ function OAuth() {
 
           <GitHubLogin
             clientId={process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID || ""}
+            redirectUri={process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URI}
             className="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-78trlr-MuiButtonBase-root-MuiIconButton-root"
             onSuccess={responseGithub}
-            onFailure={responseGithub}
+            onFailure={() => {
+              // show some errors here too
+            }}
           >
             <Image src="/github-logo.svg" alt="github" layout="fill" />
           </GitHubLogin>
