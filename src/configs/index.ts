459471640bfc15d170c5d5c62c61a1f8494f7302
Mohamadamin Karami <mohamadaminkarami.ce@gmail.com>
2022-08-20 13:40:38 +0430

    fix naming

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,26 +1,26 @@
-const get_env_variable = (
+const getEnvVariable = (
   envName: string,
   defaultValue: string = ""
 ): string => {
   return process.env[envName] || defaultValue;
 };
 
-export const SERVER_ROOT_URL = get_env_variable(
+export const SERVER_ROOT_URL = getEnvVariable(
   "NEXT_PUBLIC_SERVER_ROOT_URL",
   "https://webeloperssut.com/api"
 );
 
-export const GOOGLE_CLIENT_ID = get_env_variable(
+export const GOOGLE_CLIENT_ID = getEnvVariable(
   "NEXT_PUBLIC_GOOGLE_CLIENT_ID",
   ""
 );
 
-export const GITHUB_CLIENT_ID = get_env_variable(
+export const GITHUB_CLIENT_ID = getEnvVariable(
   "NEXT_PUBLIC_GITHUB_CLIENT_ID",
   ""
 );
 
-export const GITHUB_REDIRECT_URI = get_env_variable(
+export const GITHUB_REDIRECT_URI = getEnvVariable(
   "NEXT_PUBLIC_GITHUB_REDIRECT_URI",
   ""
 );
