86da82ffa334a952487277a91cb1204971b37778
m.behnasr <m.behnasr@pinket.com>
2022-09-02 13:55:48 +0430

    feature :  update home page

--- src/pages/dashboard-sample/index.tsx
+++ src/pages/dashboard-sample/index.tsx
@@ -3,8 +3,9 @@ import DashboardLayout from "../../components/DashboardLayout";
 import {Grid} from "@mui/material";
 import faqsData from "../../data/faqsData";
 import FaqItem from "../../components/FaqItem";
-import styles from "../../components/Home/Home.module.scss";
-
+import stylesHome from "../../components/Home/Home.module.scss";
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import styles from "./dashboard-sample.module.scss";
 
 function DashboardSamplePage() {
 
@@ -16,64 +17,70 @@ function DashboardSamplePage() {
     });
 
     return (
-    <div>
-      <DashboardLayout>
-        <Grid container spacing={2}>
-            <Grid item xs={6} sm={12}>
-                <div>
-                    اطلاعات
-                </div>
-                {faqsData.map((data, index) => (
-                    <FaqItem data={data} key={index} />
-                ))}
-            </Grid>
-            <Grid item xs={6} sm={12}>
+        <div>
+            <DashboardLayout>
+                <div className={styles.container}>
 
-                <div>
-                    وضعیت
-                </div>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <div className={styles.title}>
+                                <NotificationsIcon classes={{root: styles.titleIcon}}/>
+                                اطلاعات
+                            </div>
+                            {faqsData.map((data, index) => (
+                                <FaqItem data={data} key={index}/>
+                            ))}
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
 
-                <div>
-                    مرحله
-                </div>
-                <div>
-                    دانلود فایل سوال
-                </div>
-                <div>
-                    مهلتت پایان ۵ شهریور
-                </div>
-                <div className={styles.countdown}>
-                    <div className={styles.countdownItem}>
-                        <div className={styles.countdownValue}>
-                            {String(countdown.seconds).padStart(2, "0")}
-                        </div>
-                        <div className={styles.countdownUnit}>ثانیه</div>
-                    </div>
-                    <div className={styles.countdownItem}>
-                        <div className={styles.countdownValue}>
-                            {String(countdown.minutes).padStart(2, "0")}
-                        </div>
-                        <div className={styles.countdownUnit}>دقیقه</div>
-                    </div>
-                    <div className={styles.countdownItem}>
-                        <div className={styles.countdownValue}>
-                            {String(countdown.hours).padStart(2, "0")}
-                        </div>
-                        <div className={styles.countdownUnit}>ساعت</div>
-                    </div>
-                    <div className={styles.countdownItem}>
-                        <div className={styles.countdownValue}>
-                            {String(countdown.days).padStart(2, "0")}
-                        </div>
-                        <div className={styles.countdownUnit}>روز</div>
-                    </div>
-                </div>
+                            <div className={styles.title}>
+                                <NotificationsIcon classes={{root: styles.titleIcon}}/>
+                                وضعیت
+                            </div>
 
-            </Grid>
-        </Grid>
-      </DashboardLayout>
-    </div>
-  );
+                            <div>
+                                مرحله
+                            </div>
+                            <div>
+                                دانلود فایل سوال
+                            </div>
+                            <div>
+                                مهلتت پایان ۵ شهریور
+                            </div>
+                            <div className={stylesHome.countdown}>
+                                <div className={stylesHome.countdownItem}>
+                                    <div className={stylesHome.countdownValue}>
+                                        {String(countdown.seconds).padStart(2, "0")}
+                                    </div>
+                                    <div className={stylesHome.countdownUnit}>ثانیه</div>
+                                </div>
+                                <div className={stylesHome.countdownItem}>
+                                    <div className={stylesHome.countdownValue}>
+                                        {String(countdown.minutes).padStart(2, "0")}
+                                    </div>
+                                    <div className={stylesHome.countdownUnit}>دقیقه</div>
+                                </div>
+                                <div className={stylesHome.countdownItem}>
+                                    <div className={stylesHome.countdownValue}>
+                                        {String(countdown.hours).padStart(2, "0")}
+                                    </div>
+                                    <div className={stylesHome.countdownUnit}>ساعت</div>
+                                </div>
+                                <div className={stylesHome.countdownItem}>
+                                    <div className={stylesHome.countdownValue}>
+                                        {String(countdown.days).padStart(2, "0")}
+                                    </div>
+                                    <div className={stylesHome.countdownUnit}>روز</div>
+                                </div>
+                            </div>
+
+                        </Grid>
+                    </Grid>
+
+                </div>
+            </DashboardLayout>
+        </div>
+    );
 }
 
 export default DashboardSamplePage;
