9a6e141746d4f5c25571939af98e6cf88dfc2c13
Ali asghar Ghanati <a.a.ghanati@gmail.com>
2022-09-06 13:44:32 +0430

    - added guard for auth routes (can't see login page when already logged in)

--- src/components/AuthLayout/index.tsx
+++ src/components/AuthLayout/index.tsx
@@ -1,16 +1,25 @@
-import type { ReactElement } from "react";
+import { ReactElement, useContext } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import styles from "./AuthLayout.module.scss";
 import Image from "next/future/image";
 
 import logo from "../../assets/images/logo.png";
+import Context from "../../context/context";
 
 interface Props {
   children: ReactElement;
 }
 
 export default function Layout({ children }: Props) {
+  const router = useRouter();
+  const [context, _] = useContext(Context);
+
+  if (context.loggedIn) {
+    router.replace("/");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles["logo-container"]}>
