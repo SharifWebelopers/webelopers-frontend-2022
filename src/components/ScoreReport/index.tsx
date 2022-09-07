14143e91988e61e7d412fec01c6e1011e426e40f
navidmadannezhad <navidproject283@gmail.com>
2022-09-07 20:55:54 +0430

    ScoreReport component style done

---
+++ src/components/ScoreReport/index.tsx
@@ -0,0 +1,31 @@
+import react from "react";
+import { useEffect, useState, useRef } from "react";
+import FileDownloadRoundedIcon from '@mui/icons-material/FileDownloadRounded';
+
+import styles from "./ScoreReport.module.scss";
+import shamsiDate from "../../utils/shamsiDate";
+
+function ScoreReport(){
+
+    return ( 
+        <div className={styles.scoreReport}>
+            <div>۱</div>
+            <div className={styles.date}>
+                <p className={styles.label}>تاریخ ارسال</p>
+                <p>
+                    <span>یکشنبه</span>
+                    |
+                    <span>۶ شهریور</span>
+                    |
+                    <span>۲۳:۱۵</span>
+                </p>
+            </div>
+            <div className={styles.score}>
+                <p className={styles.label}>نمره</p>
+                <p>98/100</p>
+            </div>
+        </div>
+    )
+}
+
+export default ScoreReport;
--- src/pages/dashboard-sample/index.tsx
+++ src/pages/dashboard-sample/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import DashboardLayout from "../../components/DashboardLayout";
+import ScoreReport from "../../components/ScoreReport";
 
 function DashboardSamplePage() {
   return (
     <div>
       <DashboardLayout>
         <div style={{ padding: 32, color: "#fff" }}>سلام و درود</div>
+        <ScoreReport />
       </DashboardLayout>
     </div>
   );
