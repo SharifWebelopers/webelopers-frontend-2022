55343e29bce964871d7b917d6e21bed02af6d2a2
Ali asghar Ghanati <a.a.ghanati@gmail.com>
2022-09-09 01:28:05 +0430

    - working on adding stage to home page

--- src/components/DashboardHome/CurrentStage/index.tsx
+++ src/components/DashboardHome/CurrentStage/index.tsx
@@ -1,14 +1,55 @@
-import React from "react";
+import { Button, Divider } from "@mui/material";
+import React, { useState } from "react";
 import CurrentStageIcon from "../icons/CurrentStageIcon";
 
 import styles from "./CurrentStage.module.scss";
 
 const CurrentStage = () => {
+  const [stage, setStage] = useState({
+    name: "فلان مرحله",
+    document_url: null,
+    deadline: "۵ شهریور",
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <CurrentStageIcon />
         <div>وضعیت</div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles["stage-info"]}>
+          <div className={styles.identifier}>مرحله</div>
+          <Divider
+            orientation="vertical"
+            sx={{ color: "#c2a090", height: "60%", borderColor: "unset" }}
+          />
+          <div className={styles.data}>{stage.name}</div>
+        </div>
+        {stage.document_url ? (
+          <Button>دانلود فایل</Button>
+        ) : (
+          <div className={styles["stage-info"]}>
+            <div className={styles.data}>این مرحله فایلی ندارد!</div>
+          </div>
+        )}
+        <div className={styles["stage-info"]}>
+          <div className={styles.identifier}>مهلت پایان تا</div>
+          <Divider
+            orientation="vertical"
+            sx={{ color: "#c2a090", height: "60%", borderColor: "unset" }}
+          />
+          <div className={styles.data}>{stage.deadline}</div>
+        </div>
+        <div
+          style={{
+            border: "1px solid seagreen",
+            borderRadius: "50%",
+            height: "100%",
+            width: "218px",
+            alignSelf: "center",
+          }}
+        ></div>
       </div>
     </div>
   );
